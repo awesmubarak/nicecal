cfdd6479cc3f2205725706c1429834b84d6bcfeb
Awes Mubarak <awes.mubarak@awesmubarak.com>
2017-11-11 17:49:22 +0000

    Improve inline code documentation

--- nicecal/__init__.py
+++ nicecal/__init__.py
@@ -1,42 +1,50 @@
-# coding=utf-8
-"""Create simple calendars using box-drawing characters."""
+#!/usr/bin/env python3
+#coding=utf-8
 
-import calendar
+"""Nicecal.
 
-def month(year, month, show_month_info = 1, show_days = 1):
+Create good looking plain text calendars using box-drawing characters.
+
+"""
+
+from calendar import month as cal_month
+
+def month(year, month, show_month_info=1, show_days=1):
     """Returns a month's calendar.
-    Options:
-    -   show_month_info (default 1)
-        -   0: No month information header
-        -   1: Only month name centered as header
-        -   2: Both month name and year centered as header
-    -   show_days (defaut 1)
-        -   0: Day names are not shown
-        -   1: Day names are shown
+
+    Args:
+        show_month_info (integer) (default 1)
+            0: No month information header
+            1: Only month name centered as header
+            2: Both month name and year centered as header
+        show_days (integer) (defaut 1)
+            0: Day names are not shown
+            1: Day names are shown
+
     """
-    def wrapped_line(text):
-        """Wraps a line in bar characters."""
-        return(("│ " + text).ljust(22) + " │")
-    # Declare variables
+    # variables
     calendar_width = 22
+    raw_calendar = cal_month(year, month).split("\n")
     nice_calendar = []
-    raw_calendar  = calendar.month(year, month).split("\n")
-    # Create month and year information
+    nicer_calendar = []
+    # month and year title information
     if show_month_info == 1:
-        # Month only
-        nice_calendar.append(wrapped_line((" " * 3) + raw_calendar[0][:-5]))
+        # month
+        nice_calendar.append((" " * 3) + raw_calendar[0][:-5])
     elif show_month_info == 2:
-        # Month and year
-        nice_calendar.append(wrapped_line(raw_calendar[0]))
-    # Create day name bar
+        # month and year
+        nice_calendar.append(raw_calendar[0])
+    # day names
     if show_days:
-        nice_calendar.append(wrapped_line(raw_calendar[1]))
-    # Create main calendar
+        nice_calendar.append(raw_calendar[1])
+    # main calendar
     for line in raw_calendar[2:]:
         if line != "":
-            nice_calendar.append(wrapped_line(line))
-    # Create top and bottom bars
-    nice_calendar.insert(0, "┌" + ("─" * calendar_width) + "┐")
-    nice_calendar.append("└" + ("─" * calendar_width) + "┘")
+            nice_calendar.append(line)
+    # wrap in box drawing characters
+    for line in nice_calendar:
+        nicer_calendar.append(("│ " + line).ljust(22) + " │")
+    nicer_calendar.insert(0, "┌" + ("─" * calendar_width) + "┐")
+    nicer_calendar.append("└" + ("─" * calendar_width) + "┘")
     # Join list items and return
-    return "\n".join(nice_calendar)
+    return "\n".join(nicer_calendar)
