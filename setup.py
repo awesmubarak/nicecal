4e3a302cc590197023a21a560a09654749af56ee
Awes Mubarak <awes.mubarak@awesmubarak.com>
2017-11-12 09:44:04 +0000

    Improve setup.py formatting

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup
+#!/usr/bin/env python3
+
 from codecs import open
 from os import path
+from setuptools import setup
 
 here = path.abspath(path.dirname(__file__))
 
@@ -9,22 +11,22 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as file:
     long_description = file.read()
 
 setup(
-     name='nicecal',
-     version='1.1.1',
-     description='Create simple calendars using box-drawing characters.',
-     long_description=long_description,
-     url='https://github.com/abactel/nicecal',
-     author='abactel',
-     author_email='abactel@protonmail.com',
-     license='MIT',
-     classifiers=[
-         'Development Status :: 5 - Production/Stable',
-         'Intended Audience :: End Users/Desktop',
-         'Operating System :: OS Independent',
-         'Topic :: Utilities',
-         'License :: OSI Approved :: MIT License',
-         'Programming Language :: Python'
-     ],
-     keywords='nicecal calendar cal unicode',
-     packages=['nicecal']
+    name='nicecal',
+    version='1.1.3',
+    description='Create good looking plain text calendars using box-drawing characters.',
+    long_description=long_description,
+    url='https://github.com/awesmubarak/nicecal',
+    author='Awes Mubarak',
+    author_email='awes.mubarak@awesmubarak.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python'
+    ],
+    keywords='nicecal calendar cal unicode',
+    packages=['nicecal']
 )
